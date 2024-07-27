821a54302ac39ea9446167c1b3a029557b4129e1
Omar Ayman <omarfathyayman17@gmail.com>
2024-07-27 05:09:26 +0300

    trial

---
+++ C Programming/C Array & String/Assignment1/EX1.3.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : EX1.3.c
+ Author      : OmarAyman
+ Description : C_Basics Assignmnet1 Exercies3
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	int x,y;
+	printf("Enter two integers: ");
+	fflush(stdout);
+	scanf("%d %d",&x,&y);
+	printf("Sum : %d",x+y);
+
+
+	return 0;
+}
---
+++ C Programming/C Array & String/Assignment1/EX1.4.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : EX1.4.c
+ Author      : OmarAyman
+ Description : C_Basics Assignmnet1 Exercies4
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	float x,y;
+	printf("Enter two numbers : ");
+	fflush(stdout);
+	scanf("%f%f",&x,&y);
+	printf("Product: %f",x*y);
+
+
+	return 0;
+}
